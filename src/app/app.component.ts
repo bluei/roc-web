9d99ac9e64c70083abfbdcde4710a5d02a1de9d4
Scott Morey <scottrmorey@gmail.com>
2025-03-20 07:18:30 -0400

    updated video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +10,35 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'roc-web';
+
+  // might need to move this to the home component
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      // Access and manipulate DOM elements here
+
+      const title = document.getElementById('parallax-title');
+      const nav = document.getElementById('nav-container');
+
+      window.addEventListener('scroll', function () {
+        
+        let scrollPosition = window.pageYOffset;
+        // Adjust the speed factor here (lower = slower movement)
+        let slowScroll = scrollPosition * 0.6;
+
+        if (title)
+          title.style.transform = 'translateY(' + slowScroll + 'px)';
+
+        if (nav && scrollPosition > 10 ) {
+          nav.classList.add("solid");
+         } else {
+          nav?.classList.remove("solid")
+         }
+
+      });
+    }
+  }
 }
+
+
